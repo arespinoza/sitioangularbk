fabc5fe8653fa0c1b94452865d0473952e0ac626
arespinoza <alfredo_espi@hotmail.com>
2023-06-30 14:46:52 -0300

    subiendi

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/proyectodb';
+//const URI = 'mongodb://localhost/proyectodb';
+const URI = 'mongodb+srv://jperez:test123@cluster0.kpjec80.mongodb.net/?retryWrites=true&w=majority';
+
+
+
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
